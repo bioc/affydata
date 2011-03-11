accadb312bb48babc0fbbd48419c9cd8649ed245
Harris Jaffee <hjaffee@jhmi.edu>
2011-03-11 21:27:28 +0000

    Fixed link to GeneLogic in vignette and ?Dilution. Printed the available data object(s) at load time. Elevated Laurent from maintainer to author.  -HJ
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/affydata@1449 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .First.lib <- function(libname, pkgname) {
+  show(data(package=pkgname)$results)
   require(affy, quietly=TRUE)
   path = .path.package(pkgname)
   where <- as.environment(match(paste("package:", pkgname, sep=""), search()))
-
 }
