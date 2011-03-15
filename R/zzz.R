2cb327afa1ac3dbf4269ab7ef289e7ffa9bb1221
Harris Jaffee <hjaffee@jhmi.edu>
2011-03-15 15:05:02 +0000

    Welcome message (.First.lib).
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/affydata@1455 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,3 @@
 .First.lib <- function(libname, pkgname) {
-  show(data(package=pkgname)$results)
-  require(affy, quietly=TRUE)
-  path = .path.package(pkgname)
-  where <- as.environment(match(paste("package:", pkgname, sep=""), search()))
+    show(data(package=pkgname)$results)
 }
